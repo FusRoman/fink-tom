4f728bc6806ad2604d10e22a96ee9c680c5aca77
Roman <roman.lemontagner@gmail.com>
2023-09-14 10:15:00 +0200

    add gunicorn for prod env

--- docker/entrypoint_prod.sh
+++ docker/entrypoint_prod.sh
@@ -12,8 +12,4 @@ then
     echo "PostgreSQL started"
 fi
 
-
-conda run --no-capture-output -n fink_tom_env python fink_tom/manage.py flush --no-input
-conda run --no-capture-output -n fink_tom_env python fink_tom/manage.py migrate
-
 exec "$@"
