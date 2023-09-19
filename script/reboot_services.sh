5b13cf5c33b12aaf2137d386c4fd41bee372ed47
Roman <roman.lemontagner@gmail.com>
2023-09-19 14:11:52 +0200

    add user creation extension, add enw options in nginx.conf

--- script/reboot_services.sh
+++ script/reboot_services.sh
@@ -2,5 +2,8 @@
 
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
+docker rmi $(docker images -aq)
+docker volume rm $(docker volume ls -q)
+docker system prune --all -f
 
 docker-compose -f docker-compose.prod.yml up -d --build
